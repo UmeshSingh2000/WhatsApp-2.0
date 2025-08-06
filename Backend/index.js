fc63abab287a5e2faa7dd5f7aed48c4a76dce27c
Umesh singh <umeshsinghmehta4@gmail.com>
2025-08-06 20:37:05 +0530

    Initialize backend project with package.json and essential dependencies

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
